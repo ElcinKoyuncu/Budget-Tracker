eae39133308cc596620944b816340685f33590bf
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-07-25 12:26:34 -0500

    adding serviceworker

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+const urlsToCache = [
+    "/",
+    "/indexDb.js",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-files-v1";
+const DATA_CACHE_NAME = "data-cache-v2";
+
+self.addEventListener("install", function (event) {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+
+    if (event.request.url.includes("/api/")) {
+        console.log("called  " + event.request.url)
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
+
+    );
+});
