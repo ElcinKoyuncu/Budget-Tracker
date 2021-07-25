67c1bf1144b60c743bc7b5275ee7a2a794569608
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-07-25 15:18:52 -0500

    deploting

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
        useUnifiedTopology: true,
        useFindAndModify: false,
